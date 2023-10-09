7a06d6adf04483ffc32946b00ca8d1a855410d29
the-guy-X <143505395+the-guy-X@users.noreply.github.com>
2023-10-09 22:55:27 +0800

    Add files via upload

---
+++ switch/试试switch.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int day = 0;
+	scanf("%d", &day);//这儿不能写成scanf（"%d\n,day"）;    输入函数中不能有换行符
+	switch(day)
+	{
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+		printf("工作日");
+		break;
+	case 6:
+	case 7:
+		printf("休息日");
+		break;
+	default:
+		printf("请输入1-7之间的数字.");
+	}
+	return 0;
+}
