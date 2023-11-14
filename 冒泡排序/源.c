c20bc8df20a3dfb42ad11f277501878a94b99024
the-guy-X <143505395+the-guy-X@users.noreply.github.com>
2023-11-14 23:04:41 +0800

    Add files via upload

---
+++ 冒泡排序/源.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#define N 6
+int main()
+{
+	int x, i, j, t, flag;
+	int ch[N];
+	for (x = 0; x < N; x++)
+	{
+		scanf("%d", &ch[x]);
+	}
+	for (i = 1; i < N - 1; i++)
+	{
+		flag = 1;
+		for (j = 0; j < N - i; j++)
+		{
+			if (ch[j] > ch[j + 1])
+			{
+				flag = 0;
+				t = ch[j];
+				ch[j] = ch[j + 1];
+				ch[j + 1] = t;
+			}
+		}
+		if (flag)
+			break;
+	}
+	for (j = 0; j < N; j++)
+	{
+		printf("%d ", ch[j]);
+	}
+	return 0;
+}
